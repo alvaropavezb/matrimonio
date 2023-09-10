cea2707d0a0dd8c68dfa64f47be18987af46d4cc
alvarobriones <98062290+alvarobriones@users.noreply.github.com>
2023-09-11 08:49:44 +1200

    chore: update spotify

--- src/App.js
+++ src/App.js
@@ -226,7 +226,7 @@ const App = () => {
             buttonText={"Únete aquí"}
             onPress={() => {
               window.open(
-                "https://open.spotify.com/playlist/3g90UpMfBU2AnmuzzOZwFp?si=X036pJtOSE6Lp62JciqcNQ&pt=d76f35358d00858792c793913e939fa6"
+                "https://open.spotify.com/playlist/3g90UpMfBU2AnmuzzOZwFp?si=N8-GuhhBSAONIcR-kNT8fQ&pt=c167fe50de034e7e11073c3e7430a1f6"
               );
             }}
             footerMusic={"Se parte de nuestra playlist"}
